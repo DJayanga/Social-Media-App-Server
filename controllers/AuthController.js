bfea589dc26c7368e3f88df2d47593a45cb46ea3
DJayanga <iit17021@std.uwu.ac.lk>
2022-09-04 00:25:49 +0530

    auth router is done

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
 import UserModel from "../models/UserModel.js";
+import bcrypt from "bcrypt";
 
 //Registering a new user
 export const RegisterUser = async (req, res) => {
@@ -9,17 +10,40 @@ export const RegisterUser = async (req, res) => {
   // return res.status(400).json({ message: "User already exists" });
   // }
 
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
   const newUser = new UserModel({
     userName,
-    password,
+    password: hashedPassword,
     firstName,
     lastName,
-  })
+  });
 
   try {
-    await newUser.save()
-    res.status(200).json({ newUser })
+    await newUser.save();
+    res.status(200).json({ newUser });
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
-}
+};
+
+//Logging a user
+export const LoginUser = async (req, res) => {
+  const { userName, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ userName });
+
+    if (user) {
+      const validity = await bcrypt.compare(password, user.password);
+      validity
+        ? res.status(200).json(user)
+        : res.status(400).json("Wrong password");
+    }
+    else {
+      res.status(404).json("User does not exist");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { RegisterUser } from "../controllers/AuthController.js";
+import { RegisterUser, LoginUser } from "../controllers/AuthController.js";
 
 const router = express.Router();
 
 // router.get("/", async(req, res) => {res.send("Authenticated")});
 
 router.post('/register', RegisterUser)
+router.post('/login', LoginUser)
 
 export default router;
