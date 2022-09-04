f28b49e6625191a731b0674eff5c95b7e7531c0f
DJayanga <iit17021@std.uwu.ac.lk>
2022-09-04 11:18:24 +0530

    user router is totally done

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -62,3 +62,56 @@ export const deleteUser = async (req, res) => {
       .json("You are not authorized! You can only delete your account");
   }
 };
+
+// Follow a user
+export const followUser = async (req, res) => {
+  const id = req.params.id;
+
+  const { currentUserId } = req.body;
+
+  if (currentUserId === id) {
+    res.status(403).json("You cannot follow yourself");
+  } else {
+    try {
+      const followUser = await UserModel.findById(id);
+      const followingUser = await UserModel.findById(currentUserId);
+
+      if (!followUser.followers.includes(currentUserId)) {
+        await followUser.updateOne({ $push: { followers: currentUserId } });
+        await followingUser.updateOne({ $push: { following: id } });
+        res.status(200).json("User has been followed");
+      } else {
+        res.status(403).json("You already follow this user");
+      }
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+};
+
+
+// Un Follow a user
+export const unFollowUser = async (req, res) => {
+    const id = req.params.id;
+  
+    const { currentUserId } = req.body;
+  
+    if (currentUserId === id) {
+      res.status(403).json("You cannot unfollow yourself");
+    } else {
+      try {
+        const unFollowUser = await UserModel.findById(id);
+        const unFollowingUser = await UserModel.findById(currentUserId);
+  
+        if (unFollowUser.followers.includes(currentUserId)) {
+          await unFollowUser.updateOne({ $pull: { followers: currentUserId } });
+          await unFollowingUser.updateOne({ $pull: { following: id } });
+          res.status(200).json("User has been unfollowed");
+        } else {
+          res.status(403).json("You haven't followed this user");
+        }
+      } catch (error) {
+        res.status(500).json({ error });
+      }
+    }
+  };
